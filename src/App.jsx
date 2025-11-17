17de62a7fca64309ab0c631684d5c5cdde89b895
Malik Karaoui <malik@Maliks-MacBook-Pro.local>
2025-11-17 13:41:18 +0100

    add react routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import './index.css'
 
 export default function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-900">
+        <div className="min-h-screen w-full flex items-center justify-center bg-slate-900">
       <h1 className="text-3xl font-bold text-emerald-400">
         🍪 COOKIE arrive bientôt 🧑🏼‍🍳
       </h1>
-    </div>
+        </div>
   )
 }
